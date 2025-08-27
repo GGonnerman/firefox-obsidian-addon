5789172c90bb6011f0fdea73e94adc580536de7f
GGonnerman <gaston95g@gmail.com>
2025-08-27 07:12:33 -0500

    Added basic url fetching with fetch in sidebar. Required updating content_security_policy

--- src/pages/sidebar/Sidebar.tsx
+++ src/pages/sidebar/Sidebar.tsx
@@ -1,27 +1,12 @@
-import logo from "@assets/img/logo.svg";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Inner from "./Inner";
 
 export default function Sidebar() {
+	const queryClient = new QueryClient();
+
 	return (
-		<div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-			<header className="flex flex-col items-center justify-center text-white">
-				<img
-					src={logo}
-					className="h-36 pointer-events-none animate-spin-slow"
-					alt="logo"
-				/>
-				<p>
-					Edit <code>src/pages/sidebar/Sidebar.jsx</code> and save to reload.
-				</p>
-				<a
-					className="text-blue-400"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React!
-				</a>
-				<p>Sidebar styled with TailwindCSS!</p>
-			</header>
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<Inner />
+		</QueryClientProvider>
 	);
 }
